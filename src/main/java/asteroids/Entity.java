59227641ed31943518a31a3cbb1cb330e65d0c84
Keresztes Jonatán <keresztjoe@gmail.com>
2024-11-18 21:10:36 +0100

    Add position vector setter

--- src/main/java/asteroids/Entity.java
+++ src/main/java/asteroids/Entity.java
@@ -19,7 +19,20 @@ public abstract class Entity extends JPanel{
         pos.setY(y);
     }
 
+    /**
+     * pos vector getter
+     * @return position vector
+     */
     public Vec2 getPos(){
         return pos;
+    }
+
+    /**
+     * set the position vector
+     * @param newVec the new position vector
+     */
+    public void setPos(Vec2 newVec){
+        pos.setX(newVec.getX());
+        pos.setY(newVec.getY());
     }
 }
