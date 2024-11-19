da36d761ad65d8b0e6c55df0fd7112c3baaad60a
Keresztes Jonatán <keresztjoe@gmail.com>
2024-11-19 11:55:48 +0100

    modified:   src/main/java/asteroids/Asteroids.java

--- src/main/java/asteroids/Asteroids.java
+++ src/main/java/asteroids/Asteroids.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 
 public class Asteroids extends FlyingEntity{
     public Asteroids(Vec2 shipPos, JFrame frame){
-        super(0.0,0.0,0.0,0.0);
+        super(0.0,0.0,0.0,0.0,0.0);
         //TODO: finish the consturctor
     }
 }
